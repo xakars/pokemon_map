03c76815bb2dbf51baee355aab52c75adadc7310
Arsen <xakars02@gmail.com>
2022-09-08 18:26:59 +0300

    show pokemons on mainpage

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,20 +2,20 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
-    Image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.title
 
 
 class PokemonEntity(models.Model):
-    Pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
-    Lat = models.FloatField()
-    Lon = models.FloatField()
-    Appeared_at = models.DateTimeField(null=True, blank=True)
-    Disappeared_at = models.DateTimeField(null=True, blank=True)
-    Level = models.IntegerField(null=True, blank=True)
-    Health = models.IntegerField(null=True, blank=True)
-    Strength = models.IntegerField(null=True, blank=True)
-    Defence = models.IntegerField(null=True, blank=True)
-    Stamina = models.IntegerField(null=True, blank=True)
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    lat = models.FloatField()
+    lon = models.FloatField()
+    appeared_at = models.DateTimeField(null=True, blank=True)
+    disappeared_at = models.DateTimeField(null=True, blank=True)
+    level = models.IntegerField(null=True, blank=True)
+    health = models.IntegerField(null=True, blank=True)
+    strength = models.IntegerField(null=True, blank=True)
+    defence = models.IntegerField(null=True, blank=True)
+    stamina = models.IntegerField(null=True, blank=True)
--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -4,6 +4,8 @@ import json
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
+from pokemon_entities.models import Pokemon
+
 
 MOSCOW_CENTER = [55.751244, 37.618423]
 DEFAULT_IMAGE_URL = (
@@ -38,13 +40,13 @@ def show_all_pokemons(request):
                 pokemon_entity['lon'],
                 pokemon['img_url']
             )
-
     pokemons_on_page = []
+    pokemons = Pokemon.objects.all()
     for pokemon in pokemons:
         pokemons_on_page.append({
-            'pokemon_id': pokemon['pokemon_id'],
-            'img_url': pokemon['img_url'],
-            'title_ru': pokemon['title_ru'],
+            'pokemon_id': pokemon.id,
+            'img_url': request.build_absolute_uri(pokemon.image.url) if pokemon.image else None,
+            'title_ru': pokemon.title,
         })
 
     return render(request, 'mainpage.html', context={
